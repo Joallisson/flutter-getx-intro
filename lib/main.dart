1c5e06270e5506a7583fab06d457151755fe05a2
joallisson <joallissonnc@gmail.com>
2023-06-03 21:57:00 -0300

    60. Gerência de estado GetX - GetBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/simple/get_state.dart';
+import 'package:getx_intro/value_controller.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,54 +17,53 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class MyHomePage extends StatelessWidget {
+  MyHomePage({super.key});
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   final textController = TextEditingController();
 
-  String defineValue = '';
+  final valueController = ValueController();
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-      
             //VALOR
-            Text('Valor definido: $defineValue'),
-      
+            GetBuilder<ValueController>(
+              init: valueController,
+              builder: (controller) {
+                return Text('Valor definido: ${controller.definedValue}');
+              },
+            ),
+
             //CAMPO
             TextField(
               controller: textController,
             ),
-      
-            //BOTÃO
-            ElevatedButton(
-              onPressed: (){
-                String value = textController.text;
 
-                setState(() {
-                  defineValue = value;
-                });
-                
-              }, 
-              child: const Text("Confirmar"),
-            ),
-      
+            //BOTÃO
+            GetBuilder<ValueController>(
+              init: valueController,
+              builder: (controller) {
+              return controller.isLoading 
+              ? const CircularProgressIndicator() 
+              : ElevatedButton(
+                onPressed: () {
+                  String value = textController.text;
+                  valueController.setValue(value);
+                },
+                child: const Text("Confirmar"),
+              );
+            }),
           ],
         ),
       ),
